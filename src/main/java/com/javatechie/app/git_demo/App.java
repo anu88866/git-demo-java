06a9128f7d92733a7ad697cb6ce3e6a29bb1bb3b
anu88866 <anilreddyk999@gmail.com>
2019-02-21 10:37:56 +0530

    addedvconst cls

--- src/main/java/com/javatechie/app/git_demo/App.java
+++ src/main/java/com/javatechie/app/git_demo/App.java
@@ -1,14 +1,10 @@
 package com.javatechie.app.git_demo;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
 	public void addData(String data) {
-		System.out.println("data= "+data);
-		if(data.equals("java techie")) {
+		//System.out.println("data= "+data);
+		if(data.equals(Constant.DATA_TYPE)) {
 			System.out.println("welcome");
 		}
 	}
---
+++ src/main/java/com/javatechie/app/git_demo/Constant.java
@@ -0,0 +1,5 @@
+package com.javatechie.app.git_demo;
+
+public class Constant {
+
+}
